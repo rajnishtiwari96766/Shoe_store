ec7a901c375681db47a9f2afc085e3bd9bf1e520
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-09-24 20:02:33 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("Hello Javascript")
+
+const container2_img=document.querySelector(".container2-img")
+const items=document.querySelectorAll("li")
+
+items.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        console.log("you clicked"+index)
+        container2_img.style.transform=`translateX(${-100 * index}px)`
+    })
+})
+// console.log(container2_img)
+
+// container2_img.style.transform="translateX(100vw)"
