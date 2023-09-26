badb581de297b9b71f689b39b0af53587b2e60db
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-09-27 02:52:47 +0530

    new commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const container2_img=document.querySelector(".container2-img")
 const items=document.querySelectorAll("li")
-const products=document.querySelector(".products")
+const product_img=document.querySelector(".product_img")
+const color1=document.querySelector(".color1")
+const color2=document.querySelector(".color2")
 
 items.forEach((item,index)=>{
     item.addEventListener("click",()=>{
@@ -10,3 +12,14 @@ items.forEach((item,index)=>{
         container2_img.style.transform=`translateX(${-50 * index}vw)`
     })
 })
+
+color1.addEventListener("click",()=>{
+    console.log("black clicked") 
+    product_img.src="air.png"
+})
+
+color2.addEventListener("click",()=>{
+    console.log("blue clicked")
+    product_img.src="air2.png"
+})
+
