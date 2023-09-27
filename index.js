93e3c1fc5cc5a06434a6f1ac4989324ae95ea17a
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-09-27 23:26:57 +0530

    new commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-// console.log("Hello Javascript")
 
 const container2_img=document.querySelector(".container2-img")
 const items=document.querySelectorAll("li")
 const product_img=document.querySelectorAll(".product_img")
 const color1=document.querySelectorAll(".color1")
 const color2=document.querySelectorAll(".color2")
-// const color3=document.querySelector(".color3")
-// const color4=document.querySelector(".color4")
 
 items.forEach((item,index)=>{
     item.addEventListener("click",()=>{
